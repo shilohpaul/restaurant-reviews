23534d61a17a92058ca4a606ad689e8f278d24fc
Shiloh Paul <shilohpaul@Shilohs-MacBook-Pro.local>
2018-11-18 16:44:11 -0500

    feat: Add ARIA accessibility features

--- js/main.js
+++ js/main.js
@@ -161,6 +161,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = `picture of ${restaurant.name}`
   li.append(image);
 
   const name = document.createElement('h1');
@@ -176,7 +177,7 @@ createRestaurantHTML = (restaurant) => {
   li.append(address);
 
   const more = document.createElement('a');
-  more.innerHTML = 'View Details';
+  more.innerHTML = `View Details of ${restaurant.name}`;
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
 
