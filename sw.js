0054bf707a3d06a8e7301671e4e582553203f6d9
Shiloh Paul <shilohpaul24@gmail.com>
2018-11-19 06:03:26 -0500

    fix: Fix typos in filesToCache array

--- sw.js
+++ sw.js
@@ -1,22 +1,22 @@
 self.addEventListener('install', function(event){
   var filesToCache= [
-    css/styles.css,
-    data/restaurants.json,
-    img/1.jpg,
-    img/2.jpg,
-    img/3.jpg,
-    img/4.jpg,
-    img/5.jpg,
-    img/6.jpg,
-    img/7.jpg,
-    img/8.jpg,
-    img/9.jpg,
-    img/10.jpg,
-    index.html,
-    js/dbhelper.js,
-    js/main.js,
-    js/restaurant_info.js,
-    restaurant.html
+    'css/styles.css',
+    'data/restaurants.json',
+    'img/1.jpg',
+    'img/2.jpg',
+    'img/3.jpg',
+    'img/4.jpg',
+    'img/5.jpg',
+    'img/6.jpg',
+    'img/7.jpg',
+    'img/8.jpg',
+    'img/9.jpg',
+    'img/10.jpg',
+    'index.html',
+    'js/dbhelper.js',
+    'js/main.js',
+    'js/restaurant_info.js',
+    'restaurant.html'
   ]
   event.waitUntil(
     caches.open('restaurants-cache-v1').then(function(cache){
