a44ee107ebcad045e851a9cf9ca408eaba4aa741
Shiloh Paul <shilohpaul24@gmail.com>
2018-11-20 06:13:20 -0500

    fix: Add activation to update Cache

--- sw.js
+++ sw.js
@@ -1,39 +1,63 @@
-var restaurantCacheV1 = 'restaurant-cache-v1'
+var restaurantCache = 'restaurant-cache-v1'
 var filesToCache= [
-  'css/styles.css',
-  'data/restaurants.json',
-  'img/1.jpg',
-  'img/2.jpg',
-  'img/3.jpg',
-  'img/4.jpg',
-  'img/5.jpg',
-  'img/6.jpg',
-  'img/7.jpg',
-  'img/8.jpg',
-  'img/9.jpg',
-  'img/10.jpg',
-  'index.html',
-  'js/dbhelper.js',
-  'js/main.js',
-  'js/restaurant_info.js',
-  'restaurant.html'
-]
+  '/css/styles.css',
+  '/data/restaurants.json',
+  '/img/1.jpg',
+  '/img/2.jpg',
+  '/img/3.jpg',
+  '/img/4.jpg',
+  '/img/5.jpg',
+  '/img/6.jpg',
+  '/img/7.jpg',
+  '/img/8.jpg',
+  '/img/9.jpg',
+  '/img/10.jpg',
+  '/index.html',
+  '/js/dbhelper.js',
+  '/js/main.js',
+  '/js/restaurant_info.js',
+  '/restaurant.html'
+];
 self.addEventListener('install', function(event){
   event.waitUntil(
-    caches.open(restaurantCacheV1).then(function(cache){
+    caches.open(restaurantCache).then(function(cache){
       return cache.addAll(filesToCache);
     })
   );
+});
 
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith('restaurant-') &&
+            cacheName != restaurantCache;
+          }).map(function (cacheName) {
+            return cache.delete(cacheName);
+          })
+      );
+    })
+  );
 });
 
 self.addEventListener('fetch', function(event){
   event.respondWith(
-  caches.match(event.request).then(function(response){
-    if(response) return response;
-    return fetch(event.request);
-  }).catch(function(err){
-    console.log('Error');
-  })
+    caches.match(event.request).then(function(response){
+      if(response) {
+        return response;
+      }
+      var fetchRequest = event.request.clone();
+      return fetch(fetchRequest).then(function(response) {
+        if (!response || response.status !== 200 || response.type !== 'basic') {
+          return response;
+        }
+        var responseToCache = response.clone();
+        caches.open(restaurantCache).then(function(cache) {
+            cache.put(event.request, responseToCache);
+        });
+        return response;
+      });
+    })
   );
 });
